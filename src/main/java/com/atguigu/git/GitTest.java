0ba0900322b6520fb6762db1735e272c2c610d00
kang <1481490509@qq.com>
2022-05-22 16:17:28 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author shkstart
+ * @create 2022-05-22-16:13
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
